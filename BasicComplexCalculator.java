ece16263fbfa35e171b4ba710f9d023630506f0d
StanleySnyder <161008028+StanleySnyder@users.noreply.github.com>
2024-06-18 10:44:22 +0300

    Create BasicComplexCalculator.java

---
+++ BasicComplexCalculator.java
@@ -0,0 +1,17 @@
+public class BasicComplexCalculator implements ComplexCalculator {
+
+    @Override
+    public ComplexNumber add(ComplexNumber a, ComplexNumber b) {
+        return a.add(b);
+    }
+
+    @Override
+    public ComplexNumber multiply(ComplexNumber a, ComplexNumber b) {
+        return a.multiply(b);
+    }
+
+    @Override
+    public ComplexNumber divide(ComplexNumber a, ComplexNumber b) {
+        return a.divide(b);
+    }
+}
